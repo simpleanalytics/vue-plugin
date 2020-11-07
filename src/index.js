9e58c70a7ed5d3b9105916e2cdddce6cb20ed65e
RubenVanEldik <25854734+RubenVanEldik@users.noreply.github.com>
2020-11-07 14:44:41 +0100

    Add a global saEvent method
    Create a global saEvent method and connect it to window.sa_event

--- src/index.js
+++ src/index.js
@@ -8,31 +8,34 @@ const warn = (message) => {
     console.warn("Simple Analytics: " + message || "Something goes wrong.");
 };
 
-const injectScript = (domain) => {
+const injectScript = (vue, domain) => {
   if (typeof document === "undefined") return warn("No document defined.");
   const el = document.createElement("script");
   el.type = "text/javascript";
   el.async = true;
   el.src = "https://" + domain + "/latest.js";
   document.head.appendChild(el);
+  
+  // Add a global 'saEvent' method
+  vue.prototype.saEvent = window.sa_event
 };
 
 export default {
   install(vue, { skip = false, domain = "scripts.simpleanalyticscdn.com" }) {
-    if (skip === false) return injectScript(domain);
+    if (skip === false) return injectScript(vue, domain);
 
     // If skip is promise, resolve first. With failure always inject script
     if (isPromise(skip))
       return skip
         .then((value) => {
-          if (value !== true) return injectScript(domain);
+          if (value !== true) return injectScript(vue, domain);
           else return warn("Not sending requests because skip is active.");
         })
         .catch(injectScript);
 
     // If skip function, execute and inject when not skipping
     if (typeof skip === "function" && skip() !== true)
-      return injectScript(domain);
+      return injectScript(vue, domain);
 
     // Otherwise skip
     return warn("Not sending requests because skip is active.");
