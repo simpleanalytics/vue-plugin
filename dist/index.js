e7ad7ffb583d05baea3f234327d425232cb2e877
Adriaan van Rossum <git@adriaan.io>
2020-11-10 17:25:59 +0100

    Update to version 1.1.3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 /*!
- * simple-analytics-vue v1.1.0
+ * simple-analytics-vue v1.1.3
  * (c) 
  */
 'use strict';
@@ -13,13 +13,17 @@ var warn = function warn(message) {
   if (console && console.warn) console.warn("Simple Analytics: " + message || "Something goes wrong.");
 };
 
-var injectScript = function injectScript(domain) {
-  if (!document) return warn("No document defined.");
+var injectScript = function injectScript(vue, domain) {
+  if (typeof document === "undefined") return warn("No document defined.");
   var el = document.createElement("script");
   el.type = "text/javascript";
   el.async = true;
   el.src = "https://" + domain + "/latest.js";
-  document.head.appendChild(el);
+  document.head.appendChild(el); // Add a global 'saEvent' method when the script has been loaded
+
+  el.onload = function () {
+    vue.prototype.saEvent = window.sa_event;
+  };
 };
 
 var index = {
@@ -28,13 +32,13 @@ var index = {
         skip = _ref$skip === void 0 ? false : _ref$skip,
         _ref$domain = _ref.domain,
         domain = _ref$domain === void 0 ? "scripts.simpleanalyticscdn.com" : _ref$domain;
-    if (skip === false) return injectScript(domain); // If skip is promise, resolve first. With failure always inject script
+    if (skip === false) return injectScript(vue, domain); // If skip is promise, resolve first. With failure always inject script
 
     if (isPromise(skip)) return skip.then(function (value) {
-      if (value !== true) return injectScript(domain);else return warn("Not sending requests because skip is active.");
+      if (value !== true) return injectScript(vue, domain);else return warn("Not sending requests because skip is active.");
     }).catch(injectScript); // If skip function, execute and inject when not skipping
 
-    if (typeof skip === "function" && skip() !== true) return injectScript(domain); // Otherwise skip
+    if (typeof skip === "function" && skip() !== true) return injectScript(vue, domain); // Otherwise skip
 
     return warn("Not sending requests because skip is active.");
   }
