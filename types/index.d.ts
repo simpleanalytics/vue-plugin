f4dc6e65fc41cc0d7a7b6fce39cb79c1da8a034f
Jean P.D. Meijer <jean@analoginterface.io>
2025-08-05 21:20:37 +0200

    feat: add script options

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,6 +9,26 @@ declare global {
 export interface SimpleAnalyticsOptions {
   skip?: boolean | (() => boolean) | Promise<boolean>;
   domain?: string;
+  autoCollect?: boolean;
+  collectDnt?: boolean;
+  hostname?: string;
+  mode?: "dash";
+  ignoreMetrics?: {
+    referrer?: boolean;
+    utm?: boolean;
+    country?: boolean;
+    session?: boolean;
+    timeonpage?: boolean;
+    scrolled?: boolean;
+    useragent?: boolean;
+    screensize?: boolean;
+    viewportsize?: boolean;
+    language?: boolean;
+  };
+  ignorePages?: string[];
+  allowParams?: string[];
+  nonUniqueParams?: string[];
+  strictUtm?: boolean;
 }
 
 export declare const saEventKey: InjectionKey<(event: string) => void>;
